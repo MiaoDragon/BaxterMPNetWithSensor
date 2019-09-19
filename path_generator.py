ca5c0425c8eec936edff5314bcb077e90f82ce20
MiaoDragon <innocentmyl@gmail.com>
2019-09-19 14:00:00 -0700

    reset

--- path_generator.py
+++
@@ -1,6 +0,0 @@
-"""
-generate paths by using previously saved gazebo environments
-pseudocode:
-    1. load saved point cloud, and publish it to ROS (MoveIt topic)
-    2. path planning using the perception data in MoveIt
-"""
